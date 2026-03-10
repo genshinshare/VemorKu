b396cdeeb14f9d8039ead628d48ca96d674d442c
genshinshare <hausminum11@gmail.com>
2026-03-10 14:01:00 +0700

    Solved IMG not showing in PDF Export

--- app/Http/Controllers/View/PDF/PDFController.php
+++ app/Http/Controllers/View/PDF/PDFController.php
@@ -31,6 +31,9 @@ class PDFController extends Controller
             'vehicle_id.max' => 'Pilih vehicle id yang tersedia di kolom :attribute',
             'month.integer' => 'Pilih bulan yang tersedia di kolom :attribute'
         ]);
+        $imagePath = public_path('Logo-PT_Altrak1978.jpg');
+        $imageData = base64_encode(file_get_contents($imagePath));
+        $image = 'data:image/jpeg;base64,' . $imageData;
         $bulan = $request->month;
         $previousMonth = null;
         $previousYear = null;
@@ -115,7 +118,7 @@ class PDFController extends Controller
             }
         }
         $dompdf = new Dompdf();
-        $html = view('templatepdf.vemor', compact('department_name', 'driver_name', 'report', 'report_finance','vehicle','BULAN', 'tahun', 'plat_mobil', 'bulan', 'previousMonth', 'previousYear'));
+        $html = view('templatepdf.vemor', compact('department_name', 'driver_name', 'report', 'report_finance','vehicle','BULAN', 'tahun', 'plat_mobil', 'bulan', 'previousMonth', 'previousYear', 'image'));
         $dompdf->loadHtml($html);
         $dompdf->setPaper('folio', 'portrait');
         $dompdf->render();
