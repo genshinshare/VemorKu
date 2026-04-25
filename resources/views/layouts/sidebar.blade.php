ab453281d1dfb8aa496420d6c2a1d771a504936a
Andreas Rona Nugraha <109154486+genshinshare@users.noreply.github.com>
2026-04-26 06:47:58 +0700

    Testing Import Blade

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -108,9 +108,9 @@
                 </button>
                 <ul id="dropdown-sidebar-3" class="hidden py-2 space-y-2 bg-gray-200 border-2 border-blue-900 border border-solid border-black rounded-br-lg rounded-bl-lg">
                     <li>
-                        <a href="{{route('vemor')}}" class="flex items-center p-2 duration-300 border-transparent border-blue-900 hover:text-white rounded-[30px] hover:rounded-none hover:bg-blue-900 focus:bg-blue-600 focus:shadow-xl focus:shadow-inner focus:rounded-none group active:scale-95 focus:text-white">
+                        <a href="{{route('import')}}" class="flex items-center p-2 duration-300 border-transparent border-blue-900 hover:text-white rounded-[30px] hover:rounded-none hover:bg-blue-900 focus:bg-blue-600 focus:shadow-xl focus:shadow-inner focus:rounded-none group active:scale-95 focus:text-white">
                             <i style="width:30%; height:auto;"class="fa-solid fa-file-invoice fa-lg m-auto text-center"></i>
-                            <span style="width:70%; height:auto;" class="flex-1 m-auto text-start"> Import Excel > Database</span>
+                            <span style="width:70%; height:auto;" class="flex-1 m-auto text-start"> Import Data Excel > Database</span>
                         </a>
                     </li>
                 </ul>
