56bbc2657eda578aec7764754d3353a5c51f47ac
Andreas Rona Nugraha <109154486+genshinshare@users.noreply.github.com>
2026-04-26 07:09:56 +0700

    Testing Import Blade

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -9,9 +9,26 @@
                         </a>
                     </div>
                     <hr class="h-1 bg-gray-100 rounded md:my-3">
-                    
+                    <form action="/import/upload" method="GET" class="max-w-lg mx-auto">
+                        @csrf
+                        <label for="vehicle_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Upload Excel <i class="fa-solid fa-asterisk fa-sm" style="color: #e02424;"></i></label>
+                        <input class="cursor-pointer bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full shadow-xs placeholder:text-body" id="file_input" type="file" required>
+                        <button type="submit" id="btn-loader" class="text-white duration-300 bg-blue-600 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Import</button>
+                    </form>
                 </div>
             </div>
         </div>
     </div>
 </x-app-layout>
+<script>
+    let btnLoader = document.getElementById("btn-loader");
+
+    function changeButtonText() {
+    let buttonText = btnLoader.innerText;
+    btnLoader.innerHTML = '<i class="fa-solid fa-spinner fa-spin-pulse fa-lg" style="color: #FFFFFF;"></i>';
+    setTimeout(function() {
+        btnLoader.innerHTML = buttonText;
+    }, 3000);
+    }
+    btnLoader.addEventListener("click", changeButtonText);
+</script>
