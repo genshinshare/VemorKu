d7346ad0d9f762b8c09b0afae4d545f8e24e347f
Andreas Rona Nugraha <109154486+genshinshare@users.noreply.github.com>
2026-04-26 07:17:39 +0700

    Testing Import Blade

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -11,8 +11,13 @@
                     <hr class="h-1 bg-gray-100 rounded md:my-3">
                     <form action="/import/upload" method="GET" class="max-w-lg mx-auto">
                         @csrf
-                        <label for="vehicle_id" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Upload Excel <i class="fa-solid fa-asterisk fa-sm" style="color: #e02424;"></i></label>
-                        <input class="cursor-pointer bg-neutral-secondary-medium border border-default-medium text-heading text-sm rounded-base focus:ring-brand focus:border-brand block w-full shadow-xs placeholder:text-body" id="file_input" type="file" required>
+                        <div class="mb-6">
+                            <label for="Upload-Excel" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Upload Excel <i class="fa-solid fa-asterisk fa-sm" style="color: #e02424;"></i></label>
+                            <input class="cursor-pointer bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:text-body" id="file_input" type="file" accept=".xlsx,.xls,.csv" required>
+                            @error('year')
+                                <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-small">{{ $message }}</span></p>
+                            @enderror
+                        </div>
                         <button type="submit" id="btn-loader" class="text-white duration-300 bg-blue-600 hover:bg-blue-900 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Import</button>
                     </form>
                 </div>
