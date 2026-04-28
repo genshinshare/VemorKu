8afd64a345c646091fee72c8a29113f4f2bab3ba
Andreas Rona Nugraha <109154486+genshinshare@users.noreply.github.com>
2026-04-29 06:49:29 +0700

    Debugging Import

--- resources/views/import.blade.php
+++ resources/views/import.blade.php
@@ -13,8 +13,11 @@
                         @csrf
                         <div class="mb-6">
                             <label for="Upload-Excel" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Upload Excel <i class="fa-solid fa-asterisk fa-sm" style="color: #e02424;"></i></label>
-                            <input class="cursor-pointer bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:text-body" id="file_input" type="file" accept=".xlsx,.xls,.csv" name="file_input">
-                            @error('Upload-Excel')
+                            <input class="cursor-pointer bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 
+                            focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white 
+                            dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:text-body" id="file_input" type="file" 
+                            accept=".xlsx,.xls,.csv" name="file_input">
+                            @error('file-input')
                                 <p class="mt-2 text-sm text-red-600 dark:text-red-500"><span class="font-small">{{ $message }}</span></p>
                             @enderror
                         </div>
