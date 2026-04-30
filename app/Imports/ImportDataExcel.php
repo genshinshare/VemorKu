393b8987b2fb121703529cad58168452b3802345
Andreas Rona Nugraha <109154486+genshinshare@users.noreply.github.com>
2026-04-30 14:56:20 +0700

    Testing Debugging Import - Seluruh Sheet

--- app/Imports/ImportDataExcel.php
+++ app/Imports/ImportDataExcel.php
@@ -3,14 +3,18 @@
 namespace App\Imports;
 
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
+use Maatwebsite\Excel\Concerns\SkipsUnknownSheets;
 use App\Imports\ImportLogic;
 
-class ImportDataExcel implements WithMultipleSheets
+class ImportDataExcel implements WithMultipleSheets, SkipsUnknownSheets
 {
     public function sheets(): array
     {
-        return [
-            0 => new ImportLogic()
-        ];
+        return []; // kosongin
+    }
+
+    public function onUnknownSheet($sheetName)
+    {
+        return new ImportLogic();
     }
 }
